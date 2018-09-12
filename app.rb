cd78bc168676ba130816fb8a4ca09cc5fb40de55
Tomas Toledo <trtoledo1977@gmail.com>
2018-09-12 23:45:55 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
+require_relative 'models/piglatinizer.rb'
 
 class App < Sinatra::Base
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    @piglatinized_text = PigLatinizer.new.piglatinize(params[:user_text])
+    erb :piglatinize
+  end
+
+
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,19 @@
+class PigLatinizer
+
+    def piglatinize(text)
+        @text = text.downcase
+        vowel ='aeiou'
+        text = text.split()
+          text.map do |word|
+            if word.length == 1
+               "#{word}way"
+            elsif vowel.include? word[0].downcase
+                "#{word}way"
+            else
+                split = word.split(/([aeiou])/,2)
+                "#{split[1]}#{split[2]}#{split[0]}ay"
+            end
+        end.join(' ')
+       end
+
+end
