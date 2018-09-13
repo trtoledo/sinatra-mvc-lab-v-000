89ee2854339536358e6796200a8efb68e751137e
Tomas Toledo <trtoledo1977@gmail.com>
2018-09-13 00:18:48 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
   end
 
   post '/piglatinize' do
-    @piglatinized_text = PigLatinizer.new.piglatinize(params[:user_text])
+    @piglatinized_text = PigLatinizer.new.piglatinize(params[:user_phrase])
     erb :piglatinize
   end
 
